32749f237c15a2b9c731d587195b63e9d8ff27a1
osamma1 <155220658+osamma1@users.noreply.github.com>
2024-05-26 13:53:52 +0500

    Add files via upload

---
+++ task2.js
@@ -0,0 +1,4 @@
+var firstName = "Eric";
+var offer = "would you like to learn some Python today?";
+var message = "Hello ".concat(firstName, ", ").concat(offer);
+console.log(message);
---
+++ task2.ts
@@ -0,0 +1,5 @@
+let firstName = "Eric";
+let offer = "would you like to learn some Python today?";
+let message = `Hello ${firstName}, ${offer}`;
+
+console.log(message);
